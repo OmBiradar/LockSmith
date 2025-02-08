4845d1b45d3afab1c9bc3520c51c150033ceb447
OmBiradar <ombiradar04@gmail.com>
2025-02-08 21:25:45 +0530

    Refactor project structure and migrate password generation functionality to new package

---
+++ locksmith/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/OmBiradar/LockSmith/locaksmith/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/OmBiradar/LockSmith/cmd"
-
-func main() {
-	cmd.Execute()
-}
